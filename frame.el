dce0edf5a6e3abf43c52b19b2b3f9368e7fbe669
Nurullah Akkaya <nurullah@nakkaya.com>
2009-07-07 17:42:09 +0300

    gnus faces edited.

--- frame.el
+++ frame.el
@@ -1,14 +1,11 @@
 ;;emacs theme
 (setq frame-title-format (list "GNU Emacs " emacs-version))
-;;show current cpu load
 (setq display-time-day-and-date nil )
 (setq display-time-format "") 
 (setq display-time-load-average-threshold 0 )
 (setq display-time-string-forms '( load "," (if mail "" "")) )
 (setq display-time-interval 5)
 (display-time-mode 1)
-;(display-time-mode 0)
-;;display battery
 (setq battery-mode-line-format "%b%p%" )
 (display-battery-mode t)
 ;;mode line custom
@@ -26,12 +23,14 @@
 		(column-number-mode "C%c [")
 		global-mode-string
 		"] "
-;		user-login-name "@" ;;  you@
 		"%f"		    ;; print file with full path
 		" %-"))
 
 ;;
 ;; Theme
+;;
+;;
+;; Emacs
 ;;
 
 (setq default-frame-alist
@@ -66,63 +65,9 @@
 (set-face-background 'diff-header "#2A2A38")
 (set-face-background 'diff-file-header "#2A2A38")
 
-;;gnus theme
-;;;; Faces ;;;;
-;;;
-;; Group level faces.
-(defun my-gnus-article-prepare-hook-fn ()
-  "Function added to `gnus-article-prepare-hook'."
-  (require 'gnus-cite)                  ; NB why?
-  ;; "X wrote:" font.
-  (face-spec-set 'gnus-cite-attribution-face
-                 '((t (:slant italic :family "helv"
-                              :foreground "SkyBlue"))))
-  (face-spec-set 'gnus-cite-face-1
-                 '((((type x)) (:foreground "light blue"))
-                   (t (:foreground "turquoise"))))
-
-(face-spec-set 'gnus-cite-attribution '((t (:foreground "DarkSlateGray"))))
-(face-spec-set 'gnus-cite-1 '((t (:foreground "chartreuse4"))))
-(face-spec-set 'gnus-cite-2 '((t (:foreground "SeaGreen"))))
-(face-spec-set 'gnus-cite-3 '((t (:foreground "forest green"))))
-(face-spec-set 'gnus-cite-4 '((t (:foreground "DarkSlateGray1"))))
-(face-spec-set 'gnus-cite-5 '((t (:foreground "spring green"))))
-(face-spec-set 'gnus-cite-6 '((t (:foreground "green")))))
-(add-hook 'gnus-article-prepare-hook 'my-gnus-article-prepare-hook-fn)
-
-(defun my-gnus-article-mode-hook-fn ()
-  "Function added to `gnus-article-mode-hook'."
-  ;; Misc faces.
-  (face-spec-set 'gnus-emphasis-bold-italic
-                 '((t (:slant italic :family "helv" :weight bold))))
-
-  (face-spec-set 'gnus-emphasis-italic
-                 '((t (:slant italic :family "helv"))))
-
-  (face-spec-set 'gnus-emphasis-strikethru '((t (:strike-through t))))
-
-  (face-spec-set 'gnus-emphasis-underline-bold-italic
-                 '((t (:slant italic :family "helv" :weight bold
-                              :underline t))))
-
-  (face-spec-set 'gnus-emphasis-underline-italic
-                 '((t (:slant italic :family "helv" :underline t))))
-
-  ;; Article header faces.
-  (face-spec-set 'gnus-header-name-face '((t (:foreground "SeaGreen"))))
-
-  (face-spec-set 'gnus-header-from-face '((t (:foreground "spring green"))))
-
-  (face-spec-set 'gnus-header-newsgroups-face '((t (:foreground "yellow"))))
-
-  (face-spec-set 'gnus-header-subject-face '((t (:foreground "SeaGreen3"))))
-
-  (face-spec-set 'gnus-header-content-face '((t (:foreground "forest green"))))
-
-  (face-spec-set 'gnus-signature-face '((t (:foreground "darkslategray")))))
-
-(add-hook 'gnus-article-mode-hook 'my-gnus-article-mode-hook-fn)
-
+;;
+;; Gnus
+;;
 
 ;;group faces
 (face-spec-set 'gnus-group-mail-1 '((t (:foreground "blue1"))))
@@ -133,40 +78,50 @@
 (face-spec-set 'gnus-group-news-2-empty '((t (:foreground "royalblue"))))
 (face-spec-set 'gnus-group-news-3-empty '((t (:foreground "royalblue"))))
 
+;;citing faces
+(face-spec-set 'gnus-cite-attribution '((t (:foreground "DarkSlateGray"))))
+(face-spec-set 'gnus-cite-1 '((t (:foreground "chartreuse4"))))
+(face-spec-set 'gnus-cite-2 '((t (:foreground "SeaGreen"))))
+(face-spec-set 'gnus-cite-3 '((t (:foreground "forest green"))))
+(face-spec-set 'gnus-cite-4 '((t (:foreground "DarkSlateGray1"))))
+(face-spec-set 'gnus-cite-5 '((t (:foreground "spring green"))))
+(face-spec-set 'gnus-cite-6 '((t (:foreground "green"))))
+
+;; Article header faces.
+(face-spec-set 'gnus-header-name-face '((t (:foreground "SeaGreen"))))
+(face-spec-set 'gnus-header-from-face '((t (:foreground "spring green"))))
+(face-spec-set 'gnus-header-newsgroups-face '((t (:foreground "yellow"))))
+(face-spec-set 'gnus-header-subject-face '((t (:foreground "SeaGreen3"))))
+(face-spec-set 'gnus-header-content-face '((t (:foreground "forest green"))))
+(face-spec-set 'gnus-signature-face '((t (:foreground "darkslategray"))))
+
 ;; Summary faces.
 (face-spec-set 'gnus-summary-high-unread-face
                '((t (:foreground "firebrick" :weight bold))))
-
 (face-spec-set 'gnus-summary-normal-unread-face
                '((t (:foreground "forest green"))))
-
-
 (face-spec-set 'gnus-summary-low-unread-face
                '((t (:slant italic :family "helv"))))
-
 (face-spec-set 'gnus-summary-high-read-face
                '((t (:foreground "firebrick" :weight bold))))
-
 (face-spec-set 'gnus-summary-normal-read-face
                '((t (:foreground "steelblue"))))
 
 (set-face-foreground 'gnus-summary-high-ticked-face "firebrick")
 (set-face-foreground 'gnus-summary-high-ancient-face "firebrick")
 
+(set-face-foreground 'gnus-summary-cancelled "#888A85")
+(set-face-background 'gnus-summary-cancelled "#2A2A38")
 
-;; Eg those marked down by scoring, so should be inconspicuous.
+;;those marked down by scoring, so should be inconspicuous.
 (face-spec-set 'gnus-summary-low-read-face
                '((t (:foreground "steelblue"))))
-
 (face-spec-set 'gnus-summary-normal-ticked-face
                '((t (:foreground "firebrick"))))
-
 (face-spec-set 'gnus-summary-low-ticked-face
                '((t (:foreground "steelblue" :slant italic :family "helv"))))
-
 (face-spec-set 'gnus-summary-normal-ancient-face
                '((t (:foreground "steelblue"))))
-
 (face-spec-set 'gnus-summary-low-ancient-face
                '((t (:foreground "steelblue" :slant italic :family "helv"))))
 
@@ -174,25 +129,18 @@
 ;; Message faces.
 (face-spec-set 'message-header-name-face
                '((t (:foreground "SeaGreen"))))
-
 (face-spec-set 'message-header-to-face
                '((t (:foreground "yellow" :weight bold))))
-
 (face-spec-set 'message-header-cc-face
                '((t (:foreground "yellow"))))
-
 (face-spec-set 'message-header-subject-face
                '((t (:foreground "lawngreen"))))
-
 (face-spec-set 'message-header-other-face
                '((t (:foreground "yellow"))))
-
 (face-spec-set 'message-separator-face
                '((t (:foreground "pink"))))
-
 (face-spec-set 'message-header-newsgroups-face
                '((t (:foreground "yellow" :weight bold))))
-
 (face-spec-set 'message-cited-text-face
                '((t (:foreground "DarkSlateGray1" :weight bold))))
 
