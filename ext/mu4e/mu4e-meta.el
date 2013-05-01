1e4a3aef037c93a62653fec66af28af17a678f53
Nurullah Akkaya <nurullah@nakkaya.com>
2013-05-01 10:04:03 +0300

    add mu4e lisp files

---
+++ ext/mu4e/mu4e-meta.el
@@ -0,0 +1,8 @@
+;; auto-generated
+(defconst mu4e-mu-version "0.9.9.5"
+  "Required mu binary version; mu4e's version must agree with this.")
+
+(defconst mu4e-builddir "/home/nakkaya/Downloads/mu-0.9.9.5"
+  "Top-level build directory.")
+
+(provide 'mu4e-meta)
