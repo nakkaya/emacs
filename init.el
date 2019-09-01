d792bf651cf9f13f52c397000f31844aa1732e41
Nurullah Akkaya <nurullah@nakkaya.com>
2019-09-01 21:22:27 +0300

    switch to org mode

---
+++ init.el
@@ -0,0 +1,36 @@
+(setq dotfiles-dir
+      (file-name-directory
+       (or load-file-name (buffer-file-name))))
+
+(setq package-list
+      '(clojure-mode
+	clojure-mode-extra-font-locking
+	monroe
+	python-mode
+	matlab-mode
+	yaml-mode
+	org
+	ob-ipython
+	multi-term
+	magit
+	doom-themes))
+
+(setq package-archives
+      '(("melpa" . "http://melpa.org/packages/")
+	("gnu" . "https://elpa.gnu.org/packages/")
+	("marmalade" . "https://marmalade-repo.org/packages/")))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(require 'org)
+(require 'ob-tangle)
+
+(org-babel-load-file
+ (expand-file-name "emacs.org" dotfiles-dir))
