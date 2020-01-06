ebd5a9eea13bfbf020bc96f3dfe4c64644cfce37
Nurullah Akkaya <nurullah@nakkaya.com>
2020-01-06 21:52:47 +0200

    ubuntu configure script

---
+++ devops/ubuntu/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt-get update
+sudo apt-get install emacs26
+
+rm -f ~/.emacs
+echo "(load-file \"`pwd`/../../init.el\")" > ~/.emacs
+
+sudo adduser $USER dialout
+sudo adduser $USER dialout
+
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
