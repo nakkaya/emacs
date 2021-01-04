627e6d4acd1c1b49c4d57274b526b4eeaabd6da6
Nurullah Akkaya <nurullah@nakkaya.com>
2021-01-04 15:28:40 +0000

    Update:   devops/emacsd/emacsd   vm.el   wm.el

---
+++ wm.el
@@ -0,0 +1,30 @@
+(load-file "init.el")
+
+(setq package-list
+      '(use-package
+         exwm))
+
+(dolist (package package-list)
+  (when (not (package-installed-p package))
+    (package-install package)))
+
+(require 'exwm)
+(require 'exwm-config)
+(require 'exwm-randr)
+
+(exwm-randr-enable)
+(exwm-config-default)
+
+;;; Rename buffer to window title.
+(defun exwm-rename-buffer-to-title () (exwm-workspace-rename-buffer exwm-title))
+(add-hook 'exwm-update-title-hook 'exwm-rename-buffer-to-title)
+
+(push ?\C-x exwm-input-prefix-keys)
+(exwm-input-set-key (kbd "C-x ]") #'ibuffer)
+(exwm-input-set-key (kbd "C-x \\") #'other-window)
+
+(exwm-input-set-key
+ (kbd "C-x t")
+ (lambda () (interactive)
+   (start-process "browser" "*Messages*" "firefox")))
+
