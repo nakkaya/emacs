2e8676cdabe7cd8ac9fc262be4ff2014232513f5
Nurullah Akkaya <nurullah@nakkaya.com>
2021-07-02 12:45:37 +0300

    Update:   .github/workflows/main.yml   devops/docker/tasks.py   tasks.py

--- tasks.py
+++ tasks.py
@@ -10,14 +10,17 @@ version = subprocess.check_output(["git", "describe", "--always"]).strip().decod
 def tag(n):
     return "-t nakkaya/" + n + ":latest -t nakkaya/" + n + ":" + version
 
-def run(cmd):
+def run(cmd, dir = "."):
+    wd = os.getcwd()
+    os.chdir(dir)
     subprocess.check_call(cmd, shell=True)
+    os.chdir(wd)
 
 @task
 def build(ctx):
-    run("docker build -f Dockerfile.env -t nakkaya/env:latest .")
-    run("docker build -f Dockerfile.emacs " + tag("emacs") + " .")
-    #run("docker build --no-cache -f Dockerfile.emacs " + tag("emacs") + " .")
+    run("docker build -f Dockerfile.env -t nakkaya/env:latest .", "devops/docker/")
+    run("docker build -f Dockerfile.emacs " + tag("emacs") + " .", "devops/docker/")
+    #run("docker build --no-cache -f Dockerfile.emacs " + tag("emacs") + " ." , "devops/docker/")
 
 @task
 def push(ctx):
@@ -30,8 +33,8 @@ def pull(ctx):
 
 @task
 def up(ctx):
-    subprocess.check_call("docker-compose up -d", shell=True)
+    run("docker-compose up -d", "devops/docker/")
 
 @task
 def down(ctx):
-    subprocess.check_call("docker-compose down", shell=True)
+    run("docker-compose down", "devops/docker/")
