fe12d8bcc725a9665a5f67338065399f5868cfeb
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-14 00:35:39 +0300

    Update:   devops/docker/resources/bin/exec_gpu.sh

--- devops/docker/resources/bin/exec_gpu.sh
+++ devops/docker/resources/bin/exec_gpu.sh
@@ -5,7 +5,7 @@ if [[ -v PASSWD ]]; then
 fi
 
 if [[ -v MLFLOW_SERVER_ENB ]]; then
-    ( cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db & )
+    ( cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ & )
 fi
 
 /usr/sbin/sshd -D
