b1c91a6adfc05c5ab9a011f2205f66b306d08cc8
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-14 10:15:37 +0300

    Update:   devops/docker/resources/bin/exec_gpu.sh

--- devops/docker/resources/bin/exec_gpu.sh
+++ devops/docker/resources/bin/exec_gpu.sh
@@ -5,6 +5,12 @@ if [[ -v PASSWD ]]; then
 fi
 
 if [[ -v MLFLOW_SERVER_ENB ]]; then
+
+    if [[ -v AWS_ACCESS_KEY_ID ]]; then
+	echo 'export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID' > /home/$USER/.bashrc
+	echo 'export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY' > /home/$USER/.bashrc
+    fi
+
     ( cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ & )
 fi
 
