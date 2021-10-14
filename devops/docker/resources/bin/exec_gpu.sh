77b90ab706df69765f23b628421fc34e5d8be2fd
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-14 12:03:11 +0300

    Update:   devops/docker/resources/bin/exec_gpu.sh

--- devops/docker/resources/bin/exec_gpu.sh
+++ devops/docker/resources/bin/exec_gpu.sh
@@ -4,13 +4,12 @@ if [[ -v PASSWD ]]; then
     echo $USER:$PASSWD | sudo chpasswd
 fi
 
+if [[ -v AWS_ACCESS_KEY_ID ]]; then
+    echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /home/$USER/.bashrc
+    echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /home/$USER/.bashrc
+fi
+
 if [[ -v MLFLOW_SERVER_ENB ]]; then
-
-    if [[ -v AWS_ACCESS_KEY_ID ]]; then
-	echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /home/$USER/.bashrc
-	echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /home/$USER/.bashrc
-    fi
-
     ( cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ & )
 fi
 
