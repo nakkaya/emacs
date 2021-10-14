2bdee289ab12a22026ed1b1aabf5055b166b3934
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-14 14:27:50 +0300

    Update:   devops/docker/resources/bin/exec_gpu.sh

--- devops/docker/resources/bin/exec_gpu.sh
+++ devops/docker/resources/bin/exec_gpu.sh
@@ -10,7 +10,9 @@ if [[ -v AWS_ACCESS_KEY_ID ]]; then
 fi
 
 if [[ -v MLFLOW_SERVER_ENB ]]; then
-    ( cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ & )
+    echo "export MLFLOW_TRACKING_URI=http://127.0.0.1:8080" >> /home/$USER/.bashrc
+
+    su -c "cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ &" -m "$USER"
 fi
 
 /usr/sbin/sshd -D
