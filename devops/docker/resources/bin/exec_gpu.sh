fcc9ccb069787ade9107dea025dd6b045cc20151
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-19 20:59:11 +0300

    Update:   devops/docker/resources/bin/exec_gpu.sh

--- devops/docker/resources/bin/exec_gpu.sh
+++ devops/docker/resources/bin/exec_gpu.sh
@@ -9,9 +9,19 @@ if [[ -v AWS_ACCESS_KEY_ID ]]; then
     echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /home/$USER/.bashrc
 fi
 
-if [[ -v MLFLOW_SERVER_ENB ]]; then
-    echo "export MLFLOW_TRACKING_URI=http://127.0.0.1:8080" >> /home/$USER/.bashrc
+if [[ -v MLFLOW_TRACKING_URI ]]; then
+    echo "export MLFLOW_TRACKING_URI=${MLFLOW_TRACKING_URI}" >> /home/$USER/.bashrc
+fi
 
+if [[ -v MLFLOW_TRACKING_USERNAME ]]; then
+    echo "export MLFLOW_TRACKING_USERNAME=${MLFLOW_TRACKING_USERNAME}" >> /home/$USER/.bashrc
+fi
+
+if [[ -v MLFLOW_TRACKING_PASSWORD ]]; then
+    echo "export MLFLOW_TRACKING_PASSWORD=${MLFLOW_TRACKING_PASSWORD}" >> /home/$USER/.bashrc
+fi
+
+if [[ -v MLFLOW_SERVER_ENB ]]; then
     su -c "cd /storage && mlflow server -h 0.0.0.0 -p 8080 --backend-store-uri sqlite:///mlflow.db --default-artifact-root s3://mlflow.nakkaya.com/ &" -m "$USER"
 fi
 
