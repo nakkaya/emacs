29cbd78db1d4a4122bb90d51522d6ea1b52fbe1b
Nurullah Akkaya <nurullah@nakkaya.com>
2021-10-19 17:31:46 +0300

    Update:   devops/docker/resources/bin/ob-tangle.sh

--- devops/docker/resources/bin/ob-tangle.sh
+++ devops/docker/resources/bin/ob-tangle.sh
@@ -24,16 +24,16 @@
 (setq tramp-verbose 0
       org-id-locations-file nil)
 
-(setq tangle-host nil)
+(setq tangle-host '())
 (setq tangle-files '())
 
 (defun tangle (&rest files)
   "List of Files to Tangle."
   (setq tangle-files files))
 
-(defun host (host)
-  "Host to Tangle to."
-  (setq tangle-host host))
+(defun host (&rest  hosts)
+  "List of Hosts to Tangle to."
+  (setq tangle-hosts hosts))
 
 (defun tangle-to-host (host)
   (while (re-search-forward ":tangle\s-*?\\(.*?\\)\\($\\|\s-*?\\)" nil t)
@@ -42,16 +42,16 @@
 
 (commander
  (name "ob-tangle")
- (option "--host <host>" host nil)
+ (option "--host <*>" host)
  (option "--tangle <*>" tangle)
  (option "--help" "Show usage information" commander-print-usage))
 
-(dolist (file tangle-files)
-  (let* ((data (with-temp-buffer
-                 (insert-file-contents file)
-                 (beginning-of-buffer)
-                 (if tangle-host
-                     (tangle-to-host tangle-host))
-                 (buffer-string)))
-         (t_file (make-temp-file file nil ".org" data)))
-    (org-babel-tangle-file t_file)))
+(dolist (host tangle-hosts)
+  (dolist (file tangle-files)
+    (let* ((data (with-temp-buffer
+                   (insert-file-contents file)
+                   (beginning-of-buffer)
+                   (tangle-to-host host)
+                   (buffer-string)))
+           (t_file (make-temp-file file nil ".org" data)))
+      (org-babel-tangle-file t_file))))
