edd900984823ceb6c87ecc8185222bb89fd21b94
Nurullah Akkaya <nurullah@nakkaya.com>
2022-05-28 14:09:36 +0000

    Update:   devops/darwin/install.sh

--- devops/darwin/install.sh
+++ devops/darwin/install.sh
@@ -9,3 +9,5 @@ pip3 install jupyterlab
 
 brew tap railwaycat/emacsmacport
 brew install emacs-mac --with-native-comp --with-xwidgets --with-natural-title-bar
+
+osascript -e 'tell application "Finder" to make alias file to POSIX file "/opt/homebrew/opt/emacs-mac/Emacs.app" at POSIX file "/Applications"'
